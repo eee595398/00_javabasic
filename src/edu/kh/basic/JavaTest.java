461073b369dcdeac9e0b23238f2c842810891e52
unknown <https://github.com/eee595398>
2023-07-14 14:36:42 +0900

    first commit

---
+++ src/edu/kh/basic/JavaTest.java
@@ -0,0 +1,31 @@
+package edu.kh.basic;
+
+public class JavaTest {
+
+	
+
+	public static void main(String[] args) {
+	// main method : 자바 애플리케이션을 실행 반드시 필요한 메서드
+	
+	
+    System.out.println("Hello world");
+	System.out.println("Hello world");
+	System.out.println("@#$@#$@#$");
+	
+	// 숫자 연산
+	System.out.println(1+5);
+	
+	//""안에 작성된 코드는 단순 문자열 인식
+	//"" 안에 작성되지 않는 코드는 숫자,변수로 인식
+	
+	System.out.println(2*6/10);
+	
+	System.out.println("15*15="+(150+150) );
+	// + 기호역할
+	// -> 숫자 + 숫자 = 연산 결과
+	// -> 문자열 + 숫자 or 문자열 + 문자열 -> 이어쓰기
+	// 자바는 사칙 연산의 우선 순위를 그대로 따른다
+	// 우선계산을 원하면 괄호 사용
+	
+	}
+}
